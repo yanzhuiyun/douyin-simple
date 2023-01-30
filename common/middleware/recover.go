c0d5fdfd32aec5281eca83fb9f1bbf118fba9490
yanzhuiyun <chengma8687@gmail.com>
2023-01-30 14:46:22 +0800

    feat:add recover middleware

--- common/middleware/recover.go
+++ common/middleware/recover.go
@@ -1,1 +1,17 @@
 package middleware
+
+import (
+	"common/result"
+	"net/http"
+)
+
+func RecoverFromPanic(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				result.ResponseBadServer(w)
+			}
+		}()
+		next(w, r)
+	}
+}
