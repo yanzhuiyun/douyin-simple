47273b81020efe385596ef2a1f3c4aaddb8894b7
yanzhuiyun <chengma8687@gmail.com>
2023-02-02 16:30:00 +0800

    refacter: del func ModifyTag,add func structTomap

--- common/result/http_response.go
+++ common/result/http_response.go
@@ -12,17 +12,12 @@ import (
 type Response struct {
 	StatusCode string `json:"status_code"` // 状态码，0-成功，其他值-失败
 	StatusMsg  string `json:"status_msg"`  // 返回状态描述
-	Data       any    `json:"data,omitempty"`
 }
 
-func (r *Response) ModifyTag(tagName string) (map[string]interface{}, error) {
+func (r *Response) structTomap() (map[string]interface{}, error) {
 	out := make(map[string]interface{})
 	data, _ := json.Marshal(r)
 	json.Unmarshal(data, &out)
-	if _, ok := out["data"]; ok {
-		out[tagName] = out["data"]
-	}
-	delete(out, "data")
 	return out, nil
 }
 
@@ -30,9 +25,9 @@ func ResponseSuccess(w http.ResponseWriter, data any, tagName string) {
 	reply := &Response{
 		StatusCode: "0",
 		StatusMsg:  "SUCCESS",
-		Data:       data,
 	}
-	resp, _ := reply.ModifyTag(tagName)
+	resp, _ := reply.structTomap()
+	resp[tagName] = data
 	httpx.OkJson(w, resp)
 }
 
@@ -40,7 +35,6 @@ func ResponseBadRequest(w http.ResponseWriter, err error) {
 	reply := &Response{
 		StatusCode: xerr.ErrBadRequest,
 		StatusMsg:  err.Error(),
-		Data:       nil,
 	}
 	httpx.WriteJson(w, http.StatusBadRequest, reply)
 }
@@ -49,7 +43,6 @@ func ResponseBadServer(w http.ResponseWriter) {
 	reply := &Response{
 		StatusCode: xerr.ErrBadServer,
 		StatusMsg:  xerr.MapErrMsg(xerr.ErrBadServer),
-		Data:       nil,
 	}
 	httpx.WriteJson(w, http.StatusServiceUnavailable, reply)
 }
--- common/result/http_response_test.go
+++ common/result/http_response_test.go
@@ -25,33 +25,21 @@ func TestResponse_ModifyTag(t *testing.T) {
 			r: &Response{
 				StatusCode: "0",
 				StatusMsg:  "SUCCESS",
-				Data:       nil,
 			},
 			args:    args{"empty data"},
 			want:    map[string]interface{}{"status_code": "0", "status_msg": "SUCCESS"},
 			wantErr: false,
 		},
-		{
-			name: "test main data",
-			r: &Response{
-				StatusCode: "0",
-				StatusMsg:  "SUCCESS",
-				Data:       "main data",
-			},
-			args:    args{"maindata"},
-			want:    map[string]interface{}{"status_code": "0", "status_msg": "SUCCESS", "maindata": "main data"},
-			wantErr: false,
-		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.r.ModifyTag(tt.args.tagName)
+			got, err := tt.r.structTomap()
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Response.ModifyTag() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("Response.structTomap() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Response.ModifyTag() = %v, want %v", got, tt.want)
+				t.Errorf("Response.structTomap() = %v, want %v", got, tt.want)
 			}
 		})
 	}
